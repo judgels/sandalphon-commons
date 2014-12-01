d8c5f07beddeebaa1ef315ea7fb99a790bbf6268
Jordan Fernando <fernandojordan.92@gmail.com>
2014-12-01 16:53:10 +0700

    Refactoring and testing CRUD using models and others.
    Reviewers: ashar.fuadi
    
    Reviewed By: ashar.fuadi
    
    Subscribers: jenkins
    
    Differential Revision: http://phabricator.ia-toki.org/D52

--- app/org/iatoki/judgels/sandalphon/helpers/checkers/classes/problem/ProblemCheckAction.java
+++ app/org/iatoki/judgels/sandalphon/helpers/checkers/classes/problem/ProblemCheckAction.java
@@ -1,9 +1,9 @@
 package org.iatoki.judgels.sandalphon.helpers.checkers.classes.problem;
 
+import org.iatoki.judgels.commons.models.daos.DaoFactory;
 import org.iatoki.judgels.sandalphon.helpers.checkers.interfaces.problem.ProblemCheck;
-import org.iatoki.judgels.commons.models.dao.DaoSingletons;
-import org.iatoki.judgels.sandalphon.models.dao.interfaces.ProblemDao;
-import org.iatoki.judgels.sandalphon.models.schema.Problem;
+import org.iatoki.judgels.sandalphon.models.daos.interfaces.ProblemDao;
+import org.iatoki.judgels.sandalphon.models.domains.Problem;
 import play.libs.F;
 import play.mvc.Action;
 import play.mvc.Http;
@@ -24,7 +24,7 @@ public final class ProblemCheckAction extends Action<ProblemCheck> {
                 String problemId = path.substring(problemPos, problemPos + ((Math.min(startId.indexOf('/'), startId.length()) <= 0) ? startId.length() : Math.min(startId.indexOf('/'), startId
                         .length())));
                 if (problemId != null) {
-                    Problem problem = DaoSingletons.getInstance().getDao(ProblemDao.class).findById(problemId);
+                    Problem problem = DaoFactory.getInstance().getDao(ProblemDao.class).findById(problemId);
                     if (problem != null) {
                         context.args.put("problem", problem);
                         return delegate.call(context);
