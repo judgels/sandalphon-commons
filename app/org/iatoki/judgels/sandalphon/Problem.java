31199075245acdbe22041f92def8a485032958fe
Ashar Fuadi <fushar@gmail.com>
2015-01-06 19:58:18 +0700

    Simplify programming problem model
    Now, programming problem model and problem model are not related anymore because
    it was confusing

--- app/org/iatoki/judgels/sandalphon/Problem.java
+++ app/org/iatoki/judgels/sandalphon/Problem.java
@@ -1,19 +1,17 @@
 package org.iatoki.judgels.sandalphon;
 
-public class Problem {
+public abstract class Problem {
 
     private final long id;
     private final String jid;
     private final String name;
     private final String note;
-    private final ProblemType type;
 
-    public Problem(long id, String jid, String name, String note, ProblemType type) {
+    public Problem(long id, String jid, String name, String note) {
         this.id = id;
         this.jid = jid;
         this.name = name;
         this.note = note;
-        this.type = type;
     }
 
     public long getId() {
@@ -30,9 +28,5 @@ public class Problem {
 
     public String getNote() {
         return note;
-    }
-
-    public ProblemType getType() {
-        return type;
     }
 }
---
+++ app/org/iatoki/judgels/sandalphon/ProgrammingProblem.java
@@ -0,0 +1,8 @@
+package org.iatoki.judgels.sandalphon;
+
+public final class ProgrammingProblem extends Problem {
+
+    public ProgrammingProblem(long id, String jid, String name, String note) {
+        super(id, jid, name, note);
+    }
+}
