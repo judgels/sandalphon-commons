47ad0a36b1dd10d133a0320cac18af1dc0941910
Ashar Fuadi <fushar@gmail.com>
2015-01-07 21:54:59 +0700

    Add simple grading method view for SubtaskBatch

--- app/org/iatoki/judgels/sandalphon/ProgrammingProblem.java
+++ app/org/iatoki/judgels/sandalphon/ProgrammingProblem.java
@@ -1,8 +1,17 @@
 package org.iatoki.judgels.sandalphon;
 
+import org.iatoki.judgels.gabriel.GradingMethod;
+
 public final class ProgrammingProblem extends Problem {
 
-    public ProgrammingProblem(long id, String jid, String name, String note) {
+    private final GradingMethod gradingMethod;
+
+    public ProgrammingProblem(long id, String jid, String name, GradingMethod gradingMethod, String note) {
         super(id, jid, name, note);
+        this.gradingMethod = gradingMethod;
+    }
+
+    public GradingMethod getGradingMethod() {
+        return gradingMethod;
     }
 }
