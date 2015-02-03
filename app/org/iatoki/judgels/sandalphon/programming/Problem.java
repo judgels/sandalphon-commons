ff2b61e260eea7ca3da7f09ece07f1f60eb5fe97
Ashar Fuadi <fushar@gmail.com>
2015-02-03 15:13:50 +0700

    Introduce SubmissionService in front-end commons and implement it

--- app/org/iatoki/judgels/sandalphon/programming/Problem.java
+++ app/org/iatoki/judgels/sandalphon/programming/Problem.java
@@ -1,21 +1,21 @@
 package org.iatoki.judgels.sandalphon.programming;
 
-import org.iatoki.judgels.gabriel.GradingType;
-
 public final class Problem {
 
     private final long id;
     private final String jid;
     private final String name;
     private final String gradingType;
+    private final long timeUpdate;
 
     private final String additionalNote;
 
-    public Problem(long id, String jid, String name, String gradingType, String additionalNote) {
+    public Problem(long id, String jid, String name, String gradingType, long timeUpdate, String additionalNote) {
         this.id = id;
         this.jid = jid;
         this.name = name;
         this.gradingType = gradingType;
+        this.timeUpdate = timeUpdate;
         this.additionalNote = additionalNote;
     }
 
@@ -33,6 +33,10 @@ public final class Problem {
 
     public String getGradingType() {
         return gradingType;
+    }
+
+    public long getTimeUpdate() {
+        return timeUpdate;
     }
 
     public String getAdditionalNote() {
