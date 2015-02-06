d4436346495f640e01187fee04f4c629beb6bd89
Ashar Fuadi <fushar@gmail.com>
2015-02-06 11:17:37 +0700

    Add more info to view problem

--- app/org/iatoki/judgels/sandalphon/programming/Problem.java
+++ app/org/iatoki/judgels/sandalphon/programming/Problem.java
@@ -4,15 +4,17 @@ public final class Problem {
     private final long id;
     private final String jid;
     private final String name;
+    private final String authorJid;
     private final String gradingEngine;
     private final long timeUpdate;
 
     private final String additionalNote;
 
-    public Problem(long id, String jid, String name, String gradingEngine, long timeUpdate, String additionalNote) {
+    public Problem(long id, String jid, String name, String authorJid, String gradingEngine, long timeUpdate, String additionalNote) {
         this.id = id;
         this.jid = jid;
         this.name = name;
+        this.authorJid = authorJid;
         this.gradingEngine = gradingEngine;
         this.timeUpdate = timeUpdate;
         this.additionalNote = additionalNote;
@@ -28,6 +30,10 @@ public final class Problem {
 
     public String getName() {
         return name;
+    }
+
+    public String getAuthorJid() {
+        return authorJid;
     }
 
     public String getGradingEngine() {
