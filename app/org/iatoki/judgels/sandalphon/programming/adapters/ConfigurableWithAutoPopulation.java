302c97235ee607ec581ea0c06f799110d169f136
Ashar Fuadi <fushar@gmail.com>
2015-04-02 16:06:39 +0700

    Continue refactoring Sandalphon
    - Move all programming related attributes to file
    - Use local file system provider

--- app/org/iatoki/judgels/sandalphon/programming/adapters/ConfigurableWithAutoPopulation.java
+++ app/org/iatoki/judgels/sandalphon/programming/adapters/ConfigurableWithAutoPopulation.java
@@ -1,10 +1,11 @@
 package org.iatoki.judgels.sandalphon.programming.adapters;
 
+import org.iatoki.judgels.commons.FileInfo;
 import org.iatoki.judgels.gabriel.GradingConfig;
 
 import java.io.File;
 import java.util.List;
 
 public interface ConfigurableWithAutoPopulation {
-    GradingConfig updateConfigWithAutoPopulation(GradingConfig config, List<File> testDataFiles);
+    GradingConfig updateConfigWithAutoPopulation(GradingConfig config, List<FileInfo> testDataFiles);
 }
--- app/org/iatoki/judgels/sandalphon/programming/adapters/ConfigurableWithTokilibFormat.java
+++ app/org/iatoki/judgels/sandalphon/programming/adapters/ConfigurableWithTokilibFormat.java
@@ -1,10 +1,10 @@
 package org.iatoki.judgels.sandalphon.programming.adapters;
 
+import org.iatoki.judgels.commons.FileInfo;
 import org.iatoki.judgels.gabriel.GradingConfig;
 
-import java.io.File;
 import java.util.List;
 
 public interface ConfigurableWithTokilibFormat {
-    GradingConfig updateConfigWithTokilibFormat(GradingConfig config, List<File> testDataFiles);
+    GradingConfig updateConfigWithTokilibFormat(GradingConfig config, List<FileInfo> testDataFiles);
 }
