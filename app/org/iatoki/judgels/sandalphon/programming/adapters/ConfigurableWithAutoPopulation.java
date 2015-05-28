f6a25ff4712192fab868bf570d54f356359aacfa
Jordan Fernando <fernandojordan.92@gmail.com>
2015-05-28 15:16:17 +0700

    Adapt to changes from JophielUtils into Jophiel
    Refactor setmethod into buildInstance
    Adapt to viewAs package move.
    Optimize Imports
    
    part of ia-toki/judgels#178

--- app/org/iatoki/judgels/sandalphon/programming/adapters/ConfigurableWithAutoPopulation.java
+++ app/org/iatoki/judgels/sandalphon/programming/adapters/ConfigurableWithAutoPopulation.java
@@ -3,7 +3,6 @@ package org.iatoki.judgels.sandalphon.programming.adapters;
 import org.iatoki.judgels.commons.FileInfo;
 import org.iatoki.judgels.gabriel.GradingConfig;
 
-import java.io.File;
 import java.util.List;
 
 public interface ConfigurableWithAutoPopulation {
