bbcea4d42aa5d1898196469edf6bb771cbd6b17e
Ashar Fuadi <fushar@gmail.com>
2015-07-12 15:32:02 +0700

    Adapt to judgels-play-commons and judgels-commons restructure
    Part of ia-toki/judgels#286

--- app/org/iatoki/judgels/sandalphon/programming/adapters/ConfigurableWithTokilibFormat.java
+++ app/org/iatoki/judgels/sandalphon/programming/adapters/ConfigurableWithTokilibFormat.java
@@ -1,6 +1,6 @@
 package org.iatoki.judgels.sandalphon.programming.adapters;
 
-import org.iatoki.judgels.commons.FileInfo;
+import org.iatoki.judgels.FileInfo;
 import org.iatoki.judgels.gabriel.GradingConfig;
 
 import java.util.List;
