18634b6d6cf87c1e4ef4e0b566b8438f938f850e
Ashar Fuadi <fushar@gmail.com>
2015-07-12 15:32:02 +0700

    Adapt to judgels-play-commons and judgels-commons restructure
    Part of ia-toki/judgels#286

--- app/org/iatoki/judgels/sandalphon/programming/GradingConfigAdapter.java
+++ app/org/iatoki/judgels/sandalphon/programming/GradingConfigAdapter.java
@@ -1,6 +1,6 @@
 package org.iatoki.judgels.sandalphon.programming;
 
-import org.iatoki.judgels.commons.FileInfo;
+import org.iatoki.judgels.FileInfo;
 import org.iatoki.judgels.gabriel.GradingConfig;
 import org.iatoki.judgels.sandalphon.Problem;
 import play.data.Form;
